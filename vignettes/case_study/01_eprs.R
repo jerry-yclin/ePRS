1d71adf2db44c0a04a6b12d2b524355f191514f3
Jerry Lin <kohjerry.lin@mail.utoronto.ca>
2025-06-25 22:44:12 -0400

    Update 01_eprs.R

--- vignettes/case_study/01_eprs.R
+++ vignettes/case_study/01_eprs.R
@@ -27,9 +27,9 @@ pheno <- data.frame(STATUS = c(rep(1, 624), rep(0, 3000)), sex = rbinom(n_sample
 # --- End Placeholder ---
 
 # Load train/test indices
-train_indices <- c(sample(which(pheno$STATUS == 1), size = 312),  # 50% of cases
-                   sample(which(pheno$STATUS == 0), size = 1500)) # 50% of controls
-test_indices <- setdiff(1:nrow(pheno), train_indices)
+train_indices <- c(1:312, 625:2124)  # 50% of cases, 50% of controls
+valid_indices <- c(313:437, 2125:2724)
+test_indices <- setdiff(1:nrow(pheno), c(train_indices, valid_indices))
 
 # Load external IGE summary statistics
 # IGE_summary <- fread("generalised_epilepsy_METAL_processed.tsv")
@@ -39,9 +39,9 @@ IGE_summary <- data.frame(MarkerName = 1:n_snps, "P-value" = runif(n_snps)^2, be
 
 # --- 2. Feature Selection & Data Preparation ---
 # Optional In-sample GWAS
-gwas_pval <- array(dim=100000)
+gwas_pval <- array(dim=n_snps)
 for(i in ncol(G)){
-  mod <- summary(glm(pheno_train$STATUS ~ G[train_indices,i] + pheno_train$sex + pheno_train$pc, family="binomial"))       # Only 1 PC included for illustrative purposes 
+  mod <- summary(glm(pheno$STATUS[train_indices] ~ G[train_indices,i] + pheno$sex[train_indices] + pheno$pc[train_indices], family="binomial"))       # Only 1 PC included for illustrative purposes 
   gwas_pval[i] <- mod$coef[2,4]
 }
 
@@ -50,12 +50,14 @@ pval_thresh_filter <- 1
 snps_to_include <- which(gwas_pval < pval_thresh_filter)
 
 # Create model matrix (genotypes + covariates)
-covariates <- as.matrix(pheno[, c("sex", paste0("pc", 1:10))])
+covariates <- as.matrix(pheno[, c("sex", "pc")])
 x_data <- cbind(G[, snps_to_include], covariates)
 y_data <- pheno$STATUS
 
 x_train <- x_data[train_indices, ]
 y_train <- y_data[train_indices]
+x_valid <- x_data[valid_indices,]
+y_valid <- y_data[valid_indices,]
 x_test <- x_data[test_indices, ]
 y_test <- y_data[test_indices]
 
@@ -76,8 +78,17 @@ r_g <- 0.5
 eprs_penalty_factors <- E_j * r_g + (1 - r_g)
 eprs_penalty_factors <- c(eprs_penalty_factors, rep(0, ncol(covariates)))     # Do not penalize covariates
 
+# Tune alpha for ePRS
+alpha_thr <- c(0, 0.2, 0.4, 0.6, 0.8, 1)
+validation_cors_eprs <- sapply(alpha_thr, function(alpha_tune) {
+   m <- cv.glmnet(x_train, y_train, nfolds = 3, alpha = alpha_tune, penalty.factor = pf)
+   y_pred_valid <- predict(m, x_valid, s = "lambda.min")
+   cor(y_valid, y_pred_valid)
+    })
+best_alpha_eprs <- alpha_thr[which.max(validation_cors_eprs)]
+
 # Train the ePRS model
-cv_eprs <- cv.glmnet(x_train, y_train, family = "binomial", alpha = 0.8,      
+cv_eprs <- cv.glmnet(rbind(x_train, x_valid), c(y_train, y_valid), family = "binomial", alpha = 0.8,      
                      penalty.factor = eprs_penalty_factors)
 pred_eprs <- predict(cv_eprs, x_test, s = "lambda.min", type = "response")
 auc_eprs <- roc(y_test, as.vector(pred_eprs), quiet = TRUE)$auc
